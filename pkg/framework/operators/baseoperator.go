6b0f641878c56bf48a6a6b8f2f9e3cd6deae13be
Mikhail Krutov <mkrutov@redhat.com>
2019-11-21 15:55:03 +0100

    Teardown and Deployment support

--- pkg/framework/operators/baseoperator.go
+++ pkg/framework/operators/baseoperator.go
@@ -7,12 +7,18 @@ import (
 	"io/ioutil"
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
+	appsv1 "k8s.io/api/apps/v1"
 	apiextv1b1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	apiextension "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	clientset "k8s.io/client-go/kubernetes"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+
 	"k8s.io/client-go/rest"
 	"net/http"
 	"shipshape/pkg/framework/log"
+	"strings"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 )
 
 //All the base operator stuff goes into this class. All operator-specific things go into specific classes.
@@ -28,6 +34,12 @@ type BaseOperator struct {
 	operatorName      string
 	apiVersion        string
 	yamls             []string
+	deploymentConfig appsv1.Deployment
+	serviceAccount corev1.ServiceAccount
+	role rbacv1.Role
+	roleBinding rbacv1.RoleBinding
+	crds []apiextv1b1.CustomResourceDefinition
+	keepCRD bool
 }
 
 type DefinitionStruct struct {
@@ -37,10 +49,12 @@ type DefinitionStruct struct {
 	Spec       interface{} `json:"spec"`
 }
 
+
 func (b *BaseOperatorBuilder) NewForConfig(namespace string,
 	apiVersion string,
 	operatorName string,
 	restConfig *rest.Config,
+	keepCRD bool,
 	yamlUrls ...string) (OperatorSetup, error) {
 	baseOperator := &BaseOperator{}
 	if kubeClient, err := clientset.NewForConfig(restConfig); err != nil {
@@ -58,6 +72,7 @@ func (b *BaseOperatorBuilder) NewForConfig(namespace string,
 	baseOperator.apiVersion = apiVersion
 	baseOperator.operatorName = operatorName
 	baseOperator.yamls = yamlUrls
+	baseOperator.keepCRD = keepCRD
 	if err := baseOperator.Setup(); err != nil {
 		return nil, fmt.Errorf("failed to set up operator %s: %v", operatorName, err)
 	}
@@ -89,44 +104,47 @@ func (b *BaseOperator) errorItemLoad(failedType string, jsonObj []byte, parentEr
 }
 
 func (b *BaseOperator) errorItemCreate(failedType string, parentError error) {
-	panic(fmt.Errorf("failed to create %s : %v", failedType, parentError))
+	if strings.Contains(parentError.Error(), "already exists") {
+		//If any items already exist, don't remove those in teardown
+		b.keepCRD = true
+	} else {
+		panic(fmt.Errorf("failed to create %s : %v", failedType, parentError))
+	}
 }
 
 func (b *BaseOperator) setupServiceAccount(jsonObj []byte) {
 	log.Logf("setting up service account")
-	var account corev1.ServiceAccount
-	if err := json.Unmarshal(jsonObj, &account); err != nil {
+	if err := json.Unmarshal(jsonObj, &b.serviceAccount); err != nil {
 		b.errorItemLoad("service account", jsonObj, err)
 	}
-	if _, err := b.kubeClient.CoreV1().ServiceAccounts(b.namespace).Create(&account); err != nil {
+	if _, err := b.kubeClient.CoreV1().ServiceAccounts(b.namespace).Create(&b.serviceAccount); err != nil {
 		b.errorItemCreate("service account", err)
 	}
+
 }
 
 func (b *BaseOperator) setupRole(jsonObj []byte) {
 	log.Logf("Setting up Role")
 
-	var role rbacv1.Role
-	if err := json.Unmarshal(jsonObj, &role); err != nil {
+	if err := json.Unmarshal(jsonObj, &b.role); err != nil {
 		b.errorItemLoad("role", jsonObj, err)
 	}
-	if _, err := b.kubeClient.RbacV1().Roles(b.namespace).Create(&role); err != nil {
+	if _, err := b.kubeClient.RbacV1().Roles(b.namespace).Create(&b.role); err != nil {
 		b.errorItemCreate("role", err)
 	}
 }
 
 func (b *BaseOperator) setupRoleBinding(jsonObj []byte) {
 	log.Logf("Setting up Role Binding")
-	var roleBinding rbacv1.RoleBinding
-	if err := json.Unmarshal(jsonObj, &roleBinding); err != nil {
+	if err := json.Unmarshal(jsonObj, &b.roleBinding); err != nil {
 		b.errorItemLoad("role binding", jsonObj, err)
 	}
-	if _, err := b.kubeClient.RbacV1().RoleBindings(b.namespace).Create(&roleBinding); err != nil {
+	if _, err := b.kubeClient.RbacV1().RoleBindings(b.namespace).Create(&b.roleBinding); err != nil {
 		b.errorItemCreate("role binding", err)
 	}
 }
 
-func (b *BaseOperator) setupCRDs(jsonObj []byte) {
+func (b *BaseOperator) setupCRD(jsonObj []byte) {
 	log.Logf("Setting up CRD")
 	var CRD apiextv1b1.CustomResourceDefinition
 	if err := json.Unmarshal(jsonObj, &CRD); err != nil {
@@ -135,6 +153,7 @@ func (b *BaseOperator) setupCRDs(jsonObj []byte) {
 	if _, err := b.extClient.ApiextensionsV1beta1().CustomResourceDefinitions().Create(&CRD); err != nil {
 		b.errorItemCreate("CRD", err)
 	}
+	b.crds = append(b.crds, CRD)
 }
 
 func (b *BaseOperator) SetupYamls() error {
@@ -157,11 +176,24 @@ func (b *BaseOperator) SetupYamls() error {
 		case "RoleBinding":
 			b.setupRoleBinding(jsonItem)
 		case "CustomResourceDefinition":
-			b.setupCRDs(jsonItem)
+			b.setupCRD(jsonItem)
+		case "Deployment":
+			if err = json.Unmarshal(jsonItem, &b.deploymentConfig); err!=nil {
+				b.errorItemLoad("deployment", jsonItem, err)
+			}
 		default:
 			log.Logf("can't find item type %s", def.Kind)
 		}
 	}
+	return nil
+}
+
+func (b *BaseOperator) setupDeployment() error {
+	if _, err := b.kubeClient.AppsV1().Deployments(b.namespace).Create(&b.deploymentConfig); err!=nil {
+		b.errorItemCreate("deployment", err)
+	}
+	// Should only have single container in deployment yaml.
+	b.image = b.deploymentConfig.Spec.Template.Spec.Containers[0].Image
 	return nil
 }
 
@@ -194,16 +226,67 @@ func (b *BaseOperator) APIVersion() string {
 }
 
 func (b *BaseOperator) Setup() error {
+	log.Logf("setting up yamls: %v", b.yamls)
 	if err := b.SetupYamls(); err != nil {
+		return err
+	}
+	log.Logf("yamls setup complete, setting up deployment")
+	if err := b.setupDeployment(); err !=nil {
 		return err
 	}
 	return nil
 }
 
 func (b *BaseOperator) TeardownEach() error {
-	return nil
+	if (b.keepCRD) {
+		return nil
+	} else {
+		err := b.kubeClient.CoreV1().
+			ServiceAccounts(b.namespace).
+			Delete(b.serviceAccount.Name, metav1.NewDeleteOptions(1))
+		if err!=nil && !apierrors.IsNotFound(err){
+			return err
+		}
+		err = b.kubeClient.RbacV1().
+			Roles(b.namespace).
+			Delete(b.role.Name, metav1.NewDeleteOptions(1))
+		if err!=nil && !apierrors.IsNotFound(err){
+			return err
+		}
+		err = b.kubeClient.RbacV1().
+			RoleBindings(b.namespace).
+			Delete(b.roleBinding.Name, metav1.NewDeleteOptions(1))
+		if err!=nil && !apierrors.IsNotFound(err){
+			return err
+		}
+		err = b.kubeClient.AppsV1().
+			Deployments(b.namespace).
+			Delete(b.deploymentConfig.Name, metav1.NewDeleteOptions(1))
+		if err!=nil && !apierrors.IsNotFound(err){
+			return err
+		}
+		log.Logf("%s teradown namespace succesful", b.namespace)
+		return nil
+	}
 }
 
 func (b *BaseOperator) TeardownSuite() error {
-	return nil
+	if b.keepCRD {
+		return nil
+	} else {
+		err:= b.TeardownEach()
+		if err!=nil {
+			return err
+		}
+
+		for _, crd := range b.crds {
+			err = b.extClient.ApiextensionsV1beta1().
+				CustomResourceDefinitions().Delete(
+				crd.Name, metav1.NewDeleteOptions(1))
+			if err!=nil && !apierrors.IsNotFound(err){
+				return err
+			}
+		}
+		return nil
+	}
 }
