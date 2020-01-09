e35c44ab63bf4b9e354ed2141a6481c9ea985354
Nicolas Brignone <nbrignon@redhat.com>
2020-01-09 13:50:29 -0300

    new qe python proton client, "multireceivers"

--- pkg/api/client/amqp/qeclients/factory.go
+++ pkg/api/client/amqp/qeclients/factory.go
@@ -5,6 +5,7 @@ type AmqpQEClientImpl int
 
 const (
 	Python AmqpQEClientImpl = iota
+	MultipleReceiversPython
 	Java
 	NodeJS
 	Timeout int = 60
@@ -20,10 +21,16 @@ type AmqpQEClientImplInfo struct {
 var (
 	QEClientImageMap = map[AmqpQEClientImpl]AmqpQEClientImplInfo{
 		Python: {
-			Name:    "cli-proton-python",
-			Image:   "docker.io/rhmessagingqe/cli-proton-python:latest",
-			CommandSender: "cli-proton-python-sender",
+			Name:            "cli-proton-python",
+			Image:           "docker.io/rhmessagingqe/cli-proton-python:latest",
+			CommandSender:   "cli-proton-python-sender",
 			CommandReceiver: "cli-proton-python-receiver",
+		},
+		MultipleReceiversPython: {
+			Name:            "multiple-receivers",
+			Image:           "docker.io/nicob1987/multireceive:latest",
+			CommandSender:   "NotImplemented",
+			CommandReceiver: "./multireceive.py",
 		},
 	}
 )
