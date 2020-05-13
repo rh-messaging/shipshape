75a3ec51f47c5267a7286aef481348c1be88725e
mkrutov <48674477+mkrutov@users.noreply.github.com>
2020-05-13 15:54:11 +0200

    Update baseoperator.go
    fix log failure

--- pkg/framework/operators/baseoperator.go
+++ pkg/framework/operators/baseoperator.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/ghodss/yaml"
 	"io/ioutil"
+	 "github.com/rh-messaging/shipshape/pkg/framework/log"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
